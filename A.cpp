b44ad7e74d289d7b1fb9cd2f1ac2159bda727d9f
Anas Ibn Belal <yorder07@gmail.com>
2024-09-12 23:26:43 +0600

    first commit

---
+++ A.cpp
@@ -0,0 +1,31 @@
+////////////////              ANAS IBN BELAL            /////////////////
+//                          Template   Code                           ||
+//                                                                    ||
+//                                                                    ||
+//      BANGLADESH  UNIVERSITY OF BUSINESS AND TECHNOLOGY             ||
+//                          INTAKE 52                                 ||
+//                      DEPARTMENT OF CSE                             ||
+//                      CPS ACADEMY- BATCH-3                          ||
+//                                                                    ||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+
+void Fast_io()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+int main()
+{
+    Fast_io();
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+    }
+    return 0;
+}
